89eb16a7adb3c253af8d8dd12a6821138cb57a12
samhithatarra <samhitha.tarra@gmail.com>
2020-04-12 13:13:03 -0700

    updated init file

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import urllib, urllib.request, json, re
+from urllib.request import Request
 
 class Horoscope(MycroftSkill):
     def __init__(self):
@@ -7,15 +8,27 @@ class Horoscope(MycroftSkill):
 
     @intent_file_handler('horoscope.intent')
     def handle_horoscope(self, message):
-        sign = message.data.get('sign')
-        horoscope = ''
+        sign = message.data['sign']
+        apiUrl = "http://sandipbgt.com/theastrologer/api/horoscope/%s/today/" %sign
+        text = urllib.request.urlopen(apiUrl).read()
+        full_resp = json.loads(text)
+        final_horo = full_resp["horoscope"].split("(c)")[0]
+        
+        self.speak_dialog('horoscope', {'horoscope':final_horo})
 
-        self.speak_dialog('horoscope', data={
-            'horoscope': horoscope,
-            'sign': sign
-        })
+
+        
+        # sign = message.data.get('sign')
+        # horoscope = ''
+
+        # self.speak_dialog('horoscope', data={
+        #     'horoscope': horoscope,
+        #     'sign': sign
+        # })
 
 
 def create_skill():
     return Horoscope()
+
+
 
