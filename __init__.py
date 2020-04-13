0e91cdc65acd88cb562578bca0586b4165560e6d
samhithatarra <samhitha.tarra@gmail.com>
2020-04-12 17:51:48 -0700

    test file updated, working version

--- __init__.py
+++ __init__.py
@@ -8,18 +8,12 @@ class Horoscope(MycroftSkill):
 
     @intent_file_handler('horoscope.intent')
     def handle_horoscope(self, message):
-        sign = message.data['sign']
+        sign = message.data.get('sign')
         apiUrl = "http://sandipbgt.com/theastrologer/api/horoscope/%s/today/" %sign
         text = urllib.request.urlopen(apiUrl).read()
         full_resp = json.loads(text)
         final_horo = full_resp["horoscope"].split("(c)")[0]
-        
-        # self.speak_dialog('horoscope', {'horoscope':final_horo})
-
-
-        
-        # sign = message.data.get('sign')
-        # horoscope = ''
+   
 
         self.speak_dialog(final_horo)
 
